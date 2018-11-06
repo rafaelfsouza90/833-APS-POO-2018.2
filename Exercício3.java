51fb487a07e6be661c0260f4d20516ee5a10465e
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-11-06 19:33:16 -0200

    Aula4 - Exercício3 (while)

---
+++ Exercício3.java
@@ -0,0 +1,16 @@
+/*
+ * Faça um Loop determinado (For) que imprima uma contagem regressiva a partir do 20.
+ */
+package exercício3;
+public class Exercício3 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Contagem regressiga de 20 a 0.");
+        int n=20;
+        while(n>-1){
+            System.out.println ("Número: "+n);
+            n--;
+        }
+    }
+} 
